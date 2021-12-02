9c39f1b621ddcd313334c53e929ab1d57c5e98c5
Heather Mccartney <heather.mccartney@anaplan.com>
2021-12-02 22:19:00 +0000

    Day 2, part 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,32 +1,45 @@
-def process(filename):
-    position = 0
-    depth = 0
+def process(filename, move_fn):
+    location = {
+        'position': 0,
+        'depth': 0,
+        'aim': 0
+    }
+    
     with open(filename) as file:
         for line in file:
-            position, depth = move(*parse(line), position, depth)
-    return (position, depth)
+            location = move_fn(*parse(line), location)
+    return location['position'] * location['depth']
 
 
 def parse(command):
     return command.strip().split()[0], int(command.split()[1])
 
 
-def move(direction, amount, position=0, depth=0):
+def move_one(direction, amount, location):
     if direction == 'forward':
-        position += amount
+        location['position'] += amount
     elif direction == 'down':
-        depth += amount
+        location['depth'] += amount
     elif direction == 'up':
-        depth -= amount
+        location['depth'] -= amount
     else:
-        print(f'invalid instruction at {direction}: {amount}')
-    return (position, depth)
+        print(f'invalid instruction: {direction}: {amount}')
+    return location
 
 
-def part_one(filename):
-    position, depth = process(filename)
-    return position * depth
+def move_two(direction, amount, location):
+    if direction == 'forward':
+        location['position'] += amount
+        location['depth'] += location['aim'] * amount
+    elif direction == 'down':
+        location['aim'] += amount
+    elif direction == 'up':
+        location['aim'] -= amount
+    else:
+        print(f'invalid instruction: {direction}: {amount}')
+    return location
 
 
-print(part_one('day2/input'))
+#print(process('day2/input', move_one))
+print(process('day2/input', move_two))
 
