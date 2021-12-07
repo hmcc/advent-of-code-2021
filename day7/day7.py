10153509bb2d15ab3da5a2694a2ddbb61c3acd09
Heather Mccartney <heather.mccartney@anaplan.com>
2021-12-07 10:44:50 +0000

    Day 7, part 2

--- day7/day7.py
+++ day7/day7.py
@@ -7,15 +7,31 @@ def read_input(filename):
     return positions
 
 
-def fuel_cost(positions, position):
+def triangle(n):
+    return sum(range(n+1))
+
+
+def cost_one(positions, position):
     return sum(abs(x - position) for x in positions)
 
 
-def solve(positions):
-    costs = [fuel_cost(positions, idx) for idx, _ in enumerate(positions)]
+def cost_two(positions, position):
+    return sum(triangle(abs(x - position)) for x in positions)
+
+
+def solve(positions, cost_fn):
+    costs = [cost_fn(positions, idx) for idx, _ in enumerate(positions)]
     return min(costs)
 
 
-print(solve(read_input('day7/input')))
+def part_one(filename):
+    return solve(read_input('day7/input'), cost_one)
+
+
+def part_two(filename):
+    return solve(read_input('day7/input'), cost_two)
+
+
+print(part_two('day7/input'))
 
 
--- day7/test_day7.py
+++ day7/test_day7.py
@@ -4,8 +4,10 @@ import pytest
 from day7 import day7
 
 
-def test_fuel_cost():
-    assert day7.fuel_cost([16,1,2,0,4,2,7,1,2,14], 2) == 37
+def test_cost_one():
+    assert day7.cost_one([16,1,2,0,4,2,7,1,2,14], 2) == 37
 
 
 
+def test_cost_two():
+    assert day7.cost_two([16,1,2,0,4,2,7,1,2,14], 5) == 168
