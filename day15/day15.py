b75a65926a180983d7532c405800cc303a5a75a7
Heather Mccartney <heather.mccartney@anaplan.com>
2021-12-16 22:04:26 +0000

    Sub-second with heapdict

--- day15/day15.py
+++ day15/day15.py
@@ -1,6 +1,5 @@
-from collections import Counter
+from heapdict import heapdict
 from math import inf
-from operator import mul
 import time
 
 def read_file(filename):
@@ -17,12 +16,16 @@ def dimensions(cavern):
     return (max_x + 1, max_y + 1)
 
 
-def neighbours(x, y, max_x, max_y, visited):
+def neighbours(x, y, max_x, max_y, queue):
     possibilities = [(x-1, y), (x, y-1), (x+1, y), (x, y+1)]
     return [(x1, y1) for (x1, y1) in possibilities \
         if 0 <= x1 < max_x \
         and 0 <= y1 < max_y \
-        and (x1, y1) not in visited]
+        and (x1, y1) in queue]
+
+
+def initialise_queue(cavern):
+    return heapdict(cavern)
 
 
 def initialise_risks(cavern):
@@ -31,19 +34,16 @@ def initialise_risks(cavern):
     return risks
 
 
-def next_visit(risks, visited):
-    todos = {location: risk for location, risk in risks.items() if location not in visited}
-    counted = Counter(todos).most_common()
-    return counted[-1][0]
+def next_visit(queue):
+    return queue.popitem()
 
 
-def visit(coordinates, cavern, dim, risks, visited):
-    visited.add(coordinates)
-    for adjacent in neighbours(*coordinates, *dim, visited):
-        new_risk = risks[coordinates] + cavern[adjacent]
+def visit(coordinates, risk, cavern, cavern_dimensions, risks, queue):
+    for adjacent in neighbours(*coordinates, *cavern_dimensions, queue):
+        new_risk = risk + cavern[adjacent]
         if new_risk < risks[adjacent]:
             risks[adjacent] = new_risk
-    del cavern[coordinates]
+            queue[adjacent] = new_risk
 
 
 def answer(max_x, max_y, risks):
@@ -53,23 +53,14 @@ def answer(max_x, max_y, risks):
 def part_one(filename):
     cavern = read_file(filename)
     risks = initialise_risks(cavern)
-    visited = set()
+    queue = initialise_queue(risks)
     max_x, max_y = dimensions(cavern)
 
-    while len(visited) < max_x * max_y:
-        todo = next_visit(risks, visited)
-        visit(todo, cavern, (max_x, max_y), risks, visited)
+    while queue:
+        coordinates, risk = next_visit(queue)
+        visit(coordinates, risk, cavern, (max_x, max_y), risks, queue)
     
     return answer(max_x, max_y, risks)
-
-
-def pretty_print(risks):
-    max_x, max_y = dimensions(risks)
-    grid = [[0] * max_x for i in range(max_y)]
-    for (x, y), value in risks.items():
-        grid[y][x] = value
-    for row in grid:
-        print(''.join([str(item).ljust(5) for item in row]))
 
 
 start_time = time.time()
